341145c2e4eb32a3224be25b7485987d20333d75
Mehdi Lahmam B <mehdi@lahmam.com>
2012-12-30 00:30:09 +0100

    Add npm binaries path

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,3 +1,3 @@
 # path, the 0 in the filename causes this to load first
-export PATH=/usr/local/sbin:/usr/local/bin:$PATH:$dotfiles/bin:$dotfiles/bin/dotfiles
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/local/share/npm/bin:$PATH:$dotfiles/bin:$dotfiles/bin/dotfiles
 
